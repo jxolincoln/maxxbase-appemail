7bd5ef86f3d41458bbb16e3cffc26d0b99031c1c
jameslincoln <james@maxxsocial.com>
2018-04-30 03:22:27 -0700

    Added Custom Exceptions Added custom exceptions set for clarity in error handling. This is an incremental step to converting the library into a manageable package.

---
+++ src/AppEmail/Exceptions/EmailAttachmentException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MaxxBase\AppEmail\Exceptions;
+
+
+class EmailAttachmentException extends \Exception
+{
+    protected $message;
+    protected $code;
+    protected $file;
+    protected $line;
+
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message = "", $code, $previous);
+    }
+
+}
---
+++ src/AppEmail/Exceptions/InvalidEmailAddressException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MaxxBase\AppEmail\Exceptions;
+
+class InvalidEmailAddressException extends \Exception
+{
+    protected $message;
+    protected $code;
+    protected $file;
+    protected $line;
+
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message = "", $code, $previous);
+    }
+
+}
